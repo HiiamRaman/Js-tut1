16d321b0f5b6d2204b446ed53397df3bf0a20fde
Raman Singh Chauhan <101649164+HiiamRaman@users.noreply.github.com>
2024-05-13 08:51:57 +0000

    config done

--- .devcontainer/Basics/tut2.js
+++ .devcontainer/Basics/tut2.js
@@ -1,1 +1,14 @@
-console.log ("hello bhai!!!")
+//we are going to write examples of variable 
+//we are going to write examples of variable 
+
+let score = 5;    //here we are declaring  variable 'score'  and assigning the variable value '5'
+score = 10;
+var email = "raman.com";
+email = "binay.com";
+console.log(score);  // answer will be 5 not 10
+console.log(email);
+
+//lets talk about the constant in JS 
+const studentid = 201324
+// studentid =210232 if this will be executed it will show error
+console.log(studentid);
