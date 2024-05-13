8036dc99c823c5b7e8b85aab5dcc5f0ae49149cb
Raman Singh Chauhan <101649164+HiiamRaman@users.noreply.github.com>
2024-05-13 09:31:06 +0000

    config done

---
+++ .devcontainer/Basics/tut3.js
@@ -0,0 +1,16 @@
+// here we are going to explain about data types in JS
+"use strict"; //this will help you to use the older code of JS
+console.log (7+9) 
+//types of data type
+// 1.Numbers
+let num=12;
+let weight=80;
+//2.strings
+let color = "yellow"
+let  name = "Raman"
+//3.Boolean (this is either true or false)
+let x = true
+let y = false
+//objects
+const person = {firstname : "Raman", Lastname :"Chauhan"}
+console.table([person,num,weight,color,name,x,y])   // this helps you to print resuilt in tabular form
