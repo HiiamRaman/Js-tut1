2f8c2f53d60e97beb5848a640eb131c1e652da16
Raman Singh Chauhan <101649164+HiiamRaman@users.noreply.github.com>
2024-05-16 11:57:07 +0000

    config.done

--- .devcontainer/Basics/tut7.js
+++ .devcontainer/Basics/tut7.js
@@ -1,1 +1,9 @@
-// lets discuss  about Numbers and Scores in JS
+// lets discuss  about Numbers and Scores in JS 
+const score = 400;
+console.log(score);
+const balance = new Number(100);  //The new keyword is used in JavaScript to create a object from a constructor function
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));   //toFixed() method  is used to format a number with a specified number of decimal places
+const hundreds = 10000000000;
+console.log(hundreds.toLocaleString());
