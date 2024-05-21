42d9649cb9fae318d741e44037545fcda0d94cb4
Raman Singh Chauhan <101649164+HiiamRaman@users.noreply.github.com>
2024-05-21 09:43:32 +0000

    done

--- .devcontainer/Basics2.js/test7.js
+++ .devcontainer/Basics2.js/test7.js
@@ -21,12 +21,24 @@
 //     console.log(Regularuser.Regularuser);
 
 
-// lets talk about adding objects and function 
-const obj1 = {1:"a", 2:"b"}
-const obj2 ={3:"c",4:"d"}
-// const obj3 = Object.assign({},obj1,obj2);   // this assign is use to add two or more objects in same object
-// console.log( obj3);
+// // lets talk about adding objects and function 
+// const obj1 = {1:"a", 2:"b"}
+// const obj2 ={3:"c",4:"d"}
+// // const obj3 = Object.assign({},obj1,obj2);   // this assign is use to add two or more objects in same object
+// // console.log( obj3);
 
-//But we will use the spread format
-const obj3 = { ...obj1, ...obj2} 
-console.log(obj3);
+// //But we will use the spread format
+// const obj3 = { ...obj1, ...obj2} 
+// console.log(obj3);
+const course = {
+coursename : "JavaScript",
+price : 1000,
+instructorname:  "youtube "
+}
+// const{which one to extract} = object name          syntax
+
+const {instructorname} = course
+
+console.log(instructorname);
+
+
