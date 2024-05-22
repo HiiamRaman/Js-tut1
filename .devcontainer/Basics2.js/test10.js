ef5e3989fb767857b4e20c901af854299f9f6ec3
Raman Singh Chauhan <101649164+HiiamRaman@users.noreply.github.com>
2024-05-22 08:35:23 +0000

    done

---
+++ .devcontainer/Basics2.js/test10.js
@@ -0,0 +1,10 @@
+// lets talk about scopes in JS
+//Scope determines the accessibility (visibility) of variables.
+
+// JavaScript variables have 3 types of scope:
+//basically {} this is scope 
+// Block scope
+// Function scope
+// Global scope    
+   // we dont use var because the var is changeabble
+   
---
+++ .devcontainer/Basics2.js/test11.js
@@ -0,0 +1,22 @@
+function one () {
+    const username ="Raman";
+    function two() {
+        const website ="Youtube";
+        
+        console.log(username);
+    }
+    two()
+}
+one()
+
+
+if (true) 
+{
+    const name ="Raman Singh chauhan"
+    if(name ==="Raman Singh chauhan")
+        {
+        const website = "WWW.Chrome.com"
+        // console.log(name);
+        // console.log(website);
+    }
+}
