03143ae5949b477b1337c8ff8fa386628b4a9ab0
Raman Singh Chauhan <101649164+HiiamRaman@users.noreply.github.com>
2024-05-22 09:24:23 +0000

    done

--- .devcontainer/Basics2.js/test11.js
+++ .devcontainer/Basics2.js/test11.js
@@ -3,7 +3,7 @@ function one () {
     function two() {
         const website ="Youtube";
         
-        console.log(username);
+        // console.log(username);
     }
     two()
 }
@@ -19,4 +19,57 @@ if (true)
         // console.log(name);
         // console.log(website);
     }
+}
+
+
+//lets talk about the for loop with break statement
+   
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    // console.log(i);
+    
+}
+
+let myArray = ["flash","batman","superman"]
+// console.log(myArray.length);
+for (let i = 0; i < 3; i++) {
+    const element =myArray[i];
+    // console.log(element);
+    
+}   
+
+// lets talk about the break and continue statement
+// break statement can also be used to jump out of a loop
+
+for (let i = 0; i < 20; i++) {
+    const element = [i];
+    if(i==5) {
+        console.log(`detected 5`);
+        continue
+    }
+    console.log(element);
+    
+}
+
+
+for (let i = 1; i < 20; i++) {
+    const element = [i];
+    if(i==10){
+        // console.log(`the fav number is here ${i}`);
+        continue
+    }
+    // console.log(`value of i is ${element}`);
+    
+}
+for (let i = 0; i < 10; i++) {
+    console.log(`outer loop value ${i}`);
+    for (let j = 0; j < 10; j++) {
+        // const element = [j];
+        console.log(i ,'*',j ,'=', i*j );
+        
+    }
+ 
+    
+    
 }
